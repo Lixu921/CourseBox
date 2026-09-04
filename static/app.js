ecbecf520e51b5651db6e39ce335da6da78edffb
Lixu <2300147398@qq.com>
2026-09-04 22:24:53 +0800

    界面文案统一使用中文

--- static/app.js
+++ static/app.js
@@ -21,9 +21,9 @@ function showState(container, message, isError = false) {
 }
 
 function formatFileSize(size) {
-  if (size < 1024) return `${size} B`;
-  if (size < 1024 * 1024) return `${(size / 1024).toFixed(1)} KB`;
-  return `${(size / (1024 * 1024)).toFixed(1)} MB`;
+  if (size < 1024) return `${size} 字节`;
+  if (size < 1024 * 1024) return `${(size / 1024).toFixed(1)} 千字节`;
+  return `${(size / (1024 * 1024)).toFixed(1)} 兆字节`;
 }
 
 function courseUrl(course) {
